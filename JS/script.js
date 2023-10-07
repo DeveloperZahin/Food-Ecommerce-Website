666995a885b3594a999ae04d9760fdfc222775be
Abu Zahin Mohammad Nowsin <abuzahinmohammadnowsin@gmail.com>
2023-10-07 22:47:17 +0600

    first commit

---
+++ JS/script.js
@@ -0,0 +1,35 @@
+// Message Icon for
+
+var messageIcon = document.getElementById('message-icon');
+var messagePopup = document.getElementById('message-popup');
+
+messageIcon.addEventListener('click', function() {
+  messagePopup.classList.toggle('active');
+});
+
+
+
+
+
+
+
+
+// Select relevant HTML elements
+const filterButtons = document.querySelectorAll("#filter-buttons button");
+const filterableCards = document.querySelectorAll("#filterable-cards .cardjs");
+
+// Function to filter cards based on filter buttons
+const filterCards = (e) => {
+    document.querySelector("#filter-buttons .active").classList.remove("active");
+    e.target.classList.add("active");
+
+    filterableCards.forEach(card => {
+        // show the card if it matches the clicked filter or show all cards if "all" filter is clicked
+        if(card.dataset.name === e.target.dataset.filter || e.target.dataset.filter === "all") {
+            return card.classList.replace("hide", "show");
+        }
+        card.classList.add("hide");
+    });
+}
+
+filterButtons.forEach(button => button.addEventListener("click", filterCards));
---
+++ README.md
@@ -0,0 +1,5 @@
+# Food-Ecommerce-Website
+Responsive food e-commerce website developed by me while an intern at Iconic Solutions Limited.<br>
+<b> Special thanks Mamun bhai ||My Trainer <b> <br>
+Trainer Follow the address to see the live demo of the project-  <br>
+
